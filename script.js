406000d2b0a1f9445d38e102e62dae03cb171204
Prathameshbhujbal <bhujbal.prad@gmail.com>
2024-01-24 12:13:19 +0530

    some bug fixes

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ while (!maximum){
 let res = parseInt(Math.floor(((Math.random()) * maximum)))
 console.log(res)
 let counter = 1
-let guess = parseInt(prompt("Enter your guess: "))
+let guess = prompt("Enter your guess! (Type 'q' to quit the game) ")
 
 while (parseInt(guess) !== res && guess !== "q"){
     if (guess > res){
-        guess = prompt("Too high, guess again: ")
-        counter++
+        guess = prompt("Too high, guess again: (Type 'q' to quit the game)");
+        counter++;
     } else if (guess < res){
-        guess = prompt("Too low, guess again: ")
-        counter++
+        guess = prompt("Too low, guess again: (Type 'q' to quit the game)");
+        counter++;
+    } else {
+        guess = prompt("Enter a valid number: (Type 'q' to quit the game)");
     }
     
 }
@@ -24,6 +26,6 @@ if (parseInt(guess) === res){
 
 } 
 else {
-    alert(`You have exited the game successfully. You took ${counter} guesses.`)
+    alert(`You have exited the game successfully.`)
 
 }
